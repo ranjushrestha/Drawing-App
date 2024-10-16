97a06fc0616bb6c0539b18b4a0d73edb9c107d5b
ranju shrestha <ranjusth8844@gmail.com>
2024-10-16 13:38:58 +0545

    added color btn

--- scripts.js
+++ scripts.js
@@ -2,34 +2,34 @@ const canvas = document.getElementById('canvas');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEl = document.getElementById('size');
-
+const colorEl = document.getElementById('color');
 const ctx = canvas.getContext('2d');
 
 let size = 30;
 let isPressed = false;
+let color = 'black';
 
 canvas.addEventListener("mousedown", () => {
     isPressed = true;
 });
 
-canvas.addEventListener("mouseup", ()=> {
+canvas.addEventListener("mouseup", () => {
     isPressed = false;
 });
 
-
 canvas.addEventListener("mousemove", (e) => {
-    if (isPressed){
+    if (isPressed) {
         const x = e.offsetX;
-   const y = e.offsetY;
+        const y = e.offsetY;
 
-   drawCircle(x,y);
+        drawCircle(x, y);
     }
-   
 });
 
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.strokeStyle = color;
     ctx.stroke();
 }
 
@@ -38,25 +38,27 @@ increaseBtn.addEventListener('click', () => {
     if (size > 50) {
         size = 50;
     }
-    updateSizeOnScreen
+    updateSizeOnScreen(); // Call the function
 });
+
 decreaseBtn.addEventListener('click', () => {
     size -= 5;
-
     if (size < 5) {
         size = 5;
     }
-    updateSizeOnScreen
+    updateSizeOnScreen(); // Call the function
 });
 
+colorEl.addEventListener('change', (e) => {
+    color = e.target.value;
+});
 
 function updateSizeOnScreen() {
     sizeEl.innerText = size;
 }
-// function draw () {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     drawCircle(x, y);
-//     requestAnimationFrame(draw);
-// }
 
-// draw();
+// Initial call to set the size on screen when page loads
+
+updateSizeOnScreen();
+
+
