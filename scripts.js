630234090ff5a9ee20047dd39683d1e00487c55d
ranju shrestha <ranjusth8844@gmail.com>
2024-10-16 12:59:14 +0545

    added increase and decrease btn

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
 
 const ctx = canvas.getContext('2d');
 
 let size = 30;
-let x = 50;
-let y = 50;
+let isPressed = false;
+
+canvas.addEventListener("mousedown", () => {
+    isPressed = true;
+});
+
+canvas.addEventListener("mouseup", ()=> {
+    isPressed = false;
+});
 
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener("mousemove", (e) => {
+    if (isPressed){
+        const x = e.offsetX;
+   const y = e.offsetY;
 
+   drawCircle(x,y);
+    }
+   
 });
 
 function drawCircle(x, y) {
@@ -17,12 +33,30 @@ function drawCircle(x, y) {
     ctx.stroke();
 }
 
+increaseBtn.addEventListener('click', () => {
+    size += 5;
+    if (size > 50) {
+        size = 50;
+    }
+    updateSizeOnScreen
+});
+decreaseBtn.addEventListener('click', () => {
+    size -= 5;
+
+    if (size < 5) {
+        size = 5;
+    }
+    updateSizeOnScreen
+});
 
 
-function draw () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCircle(x, y);
-    requestAnimationFrame(draw);
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
 }
+// function draw () {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     drawCircle(x, y);
+//     requestAnimationFrame(draw);
+// }
 
-draw();
+// draw();
